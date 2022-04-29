afd9830be853b972a0975a18aa6428d3f1f00560
Juliano Fischer Naves <julianofischer@gmail.com>
2022-04-29 15:48:30 -0300

    mouseover js 2

--- mouseover/scripts.js
+++ mouseover/scripts.js
@@ -1,7 +1,28 @@
+intervalId = null;
+
 function aumentar(){
+    clearInterval(intervalId);
+    intervalId = setInterval(doIncrease, 50);
+}
+
+function doIncrease(){
+
     const sq = document.getElementById('square');
     let width = sq.offsetWidth;
     let height = sq.offsetHeight;
     sq.style.width = width + 10 + "px";
     sq.style.height = height + 10 + "px";
+}
+
+function diminuir(){
+    clearInterval(intervalId);
+    intervalId = setInterval(doDecrease, 50);
+}
+
+function doDecrease(){
+    const sq = document.getElementById('square');
+    let width = sq.offsetWidth;
+    let height = sq.offsetHeight;
+    sq.style.width = width - 10 + "px";
+    sq.style.height = height - 10 + "px";
 }
