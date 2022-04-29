a5903dc7f62545e54122ef861073c5cc88db2915
Juliano Fischer Naves <julianofischer@gmail.com>
2022-04-29 19:25:27 -0300

    todo list localstorage

--- todolist/scripts.js
+++ todolist/scripts.js
@@ -1,13 +1,44 @@
-const closebtns = document.getElementsByClassName("close");
-console.log(closebtns);
+if (!window.localStorage.currentId) {
+    window.localStorage.currentId = 1;
+}
 
+const items = { ...localStorage }
+const keys = Object.keys(items);
+Array.from(keys).forEach(key => {
+    if (key != 'currentId') {
+        const item = items[key];
+        const ul = document.getElementById("list");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(item));
+
+        const span = document.createElement("span");
+        span.appendChild(document.createTextNode("X"))
+        span.classList.add("close");
+        span.addEventListener("click", function () {
+            this.parentElement.style.display = 'none';
+        });
+        
+        li.id = key;
+        li.appendChild(span);
+        ul.appendChild(li);
+    }
+});
+
+const closebtns = document.getElementsByClassName("close");
 Array.from(closebtns).forEach(element => {
     element.addEventListener("click", function(){
         this.parentElement.remove();
+        window.localStorage.removeItem(this.parentElement.id)
     })
 });
 
-function adicionar(){
+function keyEntered(){
+    if(event.key === 'Enter'){
+        adicionar();
+    }
+}
+
+function adicionar() {
     const item = document.getElementById('item').value.trim();
     const ul = document.getElementById("list");
     var li = document.createElement("li");
@@ -16,9 +47,11 @@ function adicionar(){
     const span = document.createElement("span");
     span.appendChild(document.createTextNode("X"))
     span.classList.add("close");
-    span.addEventListener("click", function(){
+    span.addEventListener("click", function () {
         this.parentElement.style.display = 'none';
     });
     li.appendChild(span);
     ul.appendChild(li);
+    window.localStorage.setItem(window.localStorage.currentId, item);
+    window.localStorage.currentId++;
 }
