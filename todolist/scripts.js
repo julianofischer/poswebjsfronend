f92ee1ffcdac319d871fda24764b9493f29ae04a
Juliano Fischer Naves <julianofischer@gmail.com>
2022-04-29 16:54:47 -0300

    todolist js

---
+++ todolist/scripts.js
@@ -0,0 +1,24 @@
+const closebtns = document.getElementsByClassName("close");
+console.log(closebtns);
+
+Array.from(closebtns).forEach(element => {
+    element.addEventListener("click", function(){
+        this.parentElement.style.display = 'none';
+    })
+});
+
+function adicionar(){
+    const item = document.getElementById('item').value.trim();
+    const ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(item));
+
+    const span = document.createElement("span");
+    span.appendChild(document.createTextNode("X"))
+    span.addEventListener("click", function(){
+        this.parentElement.style.display = 'none';
+    });
+
+    li.appendChild(span);
+    ul.appendChild(li);
+}
